5f09cd335b8b44abcd45588250aea2086242f9e1
Gnedov Pavel <pavel@gnedov.info>
2017-10-10 15:47:45 +0700

    -

---
+++ README.md
@@ -0,0 +1,16 @@
+# Install
+
+```shell
+pip3 install pyscreenshot
+pip3 install flask
+```
+
+# Run
+
+```shell
+python3 server.py
+```
+
+# Copyright
+
+https://dustingram.com/articles/2013/05/29/vnc-screenshare-over-http-with-python
--- server.py
+++ server.py
@@ -2,13 +2,14 @@
 
 import pyscreenshot
 import flask
-from StringIO import StringIO
+import io
 
 app = flask.Flask(__name__)
 
 @app.route('/screen.png')
 def serve_pil_image():
-    img_io = StringIO()
+    # img_io = io.StringIO()
+    img_io = io.BytesIO()
     pyscreenshot.grab().save(img_io, 'PNG', quality=50)
     img_io.seek(0)
     return flask.send_file(img_io, mimetype='image/png')
@@ -18,4 +19,4 @@ def serve_img():
     return flask.render_template('screen.html')
 
 if __name__ == "__main__":
-    app.run(host= '0.0.0.0', debug=True)
+    app.run(host= '0.0.0.0', debug=False)
